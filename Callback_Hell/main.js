59421ec7d82b1df1116021c3cbc70111aea992ea
Quang Nguyen <quang13012005@gmail.com>
2023-07-05 08:34:17 +0700

    Callback Hell

---
+++ Callback_Hell/main.js
@@ -0,0 +1,61 @@
+const callback = (error, data) => {
+    if (error) {
+        console.log("calling callback", error, data);
+    }
+
+    if (data) {
+        console.log("calling callback with data", error, data);
+    }
+}
+
+function getToDo(id, callback) {
+    var xhttp = new XMLHttpRequest();
+
+    //Get data
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && xhttp.status === 200) {
+            const data = JSON.parse(xhttp.responseText);
+            const dataString = JSON.stringify(data);
+            callback(undefined, data);
+            callback(undefined, dataString);
+        }
+
+        if (this.readyState === 4 && xhttp.status != 200) {
+            callback('something wrong', undefined);
+        }
+
+    };
+
+    xhttp.open("GET", `https://jsonplaceholder.typicode.com/todos/${id}`, true);
+    xhttp.send();
+
+}
+
+getToDo(1, (error, data) => {
+    if (error) {
+        console.log('>>> calling callback with errors: ', error);
+    }
+    if (data) {
+        console.log('>>> calling callback with data: ', data);
+
+        getToDo(2, () => {
+            if (error) {
+                console.log('>>> calling back with error', error);
+            }
+
+            if (data) {
+                console.log('>>>> Calling callback with ', data);
+
+                getToDo(3, () => {
+                    if (error) {
+                        console.log('>>> calling call back with error', error);
+                    }
+
+                    if (data) {
+                        console.log('>>> calling call back with data', data);
+                    }
+                });
+            }
+        });
+    }
+});
